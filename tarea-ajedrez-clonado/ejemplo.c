3b26370b323bf865688f73083ae41e8ae67efe8e
diegoanampa <ddanampa@gmail.com>
2022-06-15 02:58:01 -0500

    ejericio 3

--- tarea-ajedrez-clonado/ejemplo.c
+++ tarea-ajedrez-clonado/ejemplo.c
@@ -2,7 +2,13 @@
 #include "figures.h"
 
 void display(){
-  char** blackKnight = join(blackSquare,whiteSquare);
-  char** blackKnight2 = repeatH(blackKnight,4);
-  interpreter(blackKnight2);
+  char** unionBW = join(blackSquare,whiteSquare);
+  char** repetirUBW = repeatH(unionBW,4);
+  char** unionWB = join(whiteSquare,blackSquare);
+  char** repetirUWB = repeatH(unionWB,4);
+  char** unionF1F2 = up(repetirUBW,repetirUWB);
+  char** repetirUF1F2 = repeatV(unionF1F2,2);
+	  
+  interpreter(repetirUF1F2);
+  
 }
