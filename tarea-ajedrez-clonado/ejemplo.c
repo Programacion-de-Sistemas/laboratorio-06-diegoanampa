176d65a1e8c9b648e2ca7d7f5716f494039c183d
diegoanampa <ddanampa@gmail.com>
2022-06-16 22:08:10 -0500

    utilizando up para unir filas

--- tarea-ajedrez-clonado/ejemplo.c
+++ tarea-ajedrez-clonado/ejemplo.c
@@ -9,8 +9,20 @@ void display(){
   char** unionF1F2 = up(repetirUBW,repetirUWB);
   char** repetirUF1F2 = repeatV(unionF1F2,2);
   
-  char** var1 = join(greyBGbishop,king);
+  char** union1 = join(greyBGrook,knight);
+  char** union2 = join(union1,greyBGbishop);
+  char** union3 = join(union2,queen);
+  char** union4 = join(union3,greyBGking);
+  char** union5 = join(union4,bishop);
+  char** union6 = join(union5,greyBGknight);
+  char** union7 = join(union6,rook);
+  
+  char** union8 = join(pawn,greyBGpawn);
+  char** peonesBlancos = repeatH(union8,4);
+  char** union9 = up(union7,peonesBlancos);
+  
+  char** union10 = up(union9,repetirUF1F2);
 	  
-  interpreter(var1);
+  interpreter(union10);
   
 }
